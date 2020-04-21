6a7bc644b4f41d7bdbadf6b1ea5ee68346f3912e
armnutthapon <armnutthapon@hotmail.co.th>
2020-04-22 02:44:24 +0700

    newer

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-$(function () {
+/*$(function () {
   var url = "https://pomber.github.io/covid19/timeseries.json";
   $.getJSON(url, function (result) {
     console.log(result);
@@ -11,5 +11,38 @@ $(function () {
       $("#data").append(row);
       no++;
     }
+  });
+});
+*/
+
+
+$(function () {
+  var url = "https://pomber.github.io/covid19/timeseries.json";
+  $.getJSON(url, function (result) {
+    console.log(result);
+    var no = 1;
+    var confirmed = 0;
+    var deaths = 0;
+    var recovered = 0;
+    for (var country in result) {
+      var row = `<tr>
+                          <th scope="row">${no}</th>
+                          <td><a href="country2.html?country=${country}">${country}</a></td>
+                   </tr>`;
+      $("#data").append(row);
+      no++;
+    }
+    for (var country in result) {
+      var selectedCountry = result[country];
+      var total = selectedCountry.length;
+      confirmed = confirmed + selectedCountry[total - 1].confirmed;
+      deaths = deaths + selectedCountry[total - 1].deaths;
+      recovered = recovered + selectedCountry[total - 1].recovered;
+    }
+    $("#confirmed").append(confirmed);
+    $("#deaths").append(deaths);
+    $("#recovered").append(recovered);
+    console.log(confirmed);
+    
   });
 });
