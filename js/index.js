b2df3feab1674d88b1537a5c0edffd065035f60e
armnutthapon <armnutthapon@hotmail.co.th>
2020-04-27 03:24:46 +0700

    เสร็จ

--- js/index.js
+++ js/index.js
@@ -130,7 +130,7 @@ $(function () {
           datasets: [{
             label: "Confirmed : ",
             lineTension: 0.3,
-            backgroundColor: "rgba(246, 194, 62, 0.5)",
+            backgroundColor: "rgba(255, 255, 255, 0)",
             borderColor: "rgba(246, 194, 62, 1)",
             pointRadius: 3,
             pointBackgroundColor: "rgba(78, 115, 223, 1)",
@@ -141,87 +141,10 @@ $(function () {
             pointHitRadius: 10,
             pointBorderWidth: 2,
             data: [totalconfirmed6, totalconfirmed5, totalconfirmed4, totalconfirmed3, totalconfirmed2, totalconfirmed1],
-          }],
-        },
-        options: {
-          maintainAspectRatio: false,
-          layout: {
-            padding: {
-              left: 10,
-              right: 25,
-              top: 25,
-              bottom: 0
-            }
-          },
-          scales: {
-            xAxes: [{
-              time: {
-                unit: 'date'
-              },
-              gridLines: {
-                display: false,
-                drawBorder: false
-              },
-              ticks: {
-                maxTicksLimit: 7
-              }
-            }],
-            yAxes: [{
-              ticks: {
-                maxTicksLimit: 5,
-                padding: 10,
-                // Include a dollar sign in the ticks
-                callback: function (value, index, values) {
-                  return number_format(value);
-                }
-              },
-              gridLines: {
-                color: "rgb(234, 236, 244)",
-                zeroLineColor: "rgb(234, 236, 244)",
-                drawBorder: false,
-                borderDash: [2],
-                zeroLineBorderDash: [2]
-              }
-            }],
-          },
-          legend: {
-            display: false
-          },
-          tooltips: {
-            backgroundColor: "rgb(255,255,255)",
-            bodyFontColor: "#858796",
-            titleMarginBottom: 10,
-            titleFontColor: '#6e707e',
-            titleFontSize: 14,
-            borderColor: '#dddfeb',
-            borderWidth: 1,
-            xPadding: 15,
-            yPadding: 15,
-            displayColors: false,
-            intersect: false,
-            mode: 'index',
-            caretPadding: 10,
-            callbacks: {
-              label: function (tooltipItem, chart) {
-                var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
-                return datasetLabel + number_format(tooltipItem.yLabel);
-              }
-            }
-          }
-        }
-      });
-
-
-
-      var ctx = document.getElementById("myAreaChart2");
-      var myLineChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-          labels: [totalweek6, totalweek5, totalweek4, totalweek3, totalweek2, totalweek1],
-          datasets: [{
-            label: "Confirmed : ",
+          },{
+            label: "Recovered : ",
             lineTension: 0.3,
-            backgroundColor: "rgba(28, 200, 138, 0.5)",
+            backgroundColor: "rgba(255, 255, 255, 0)",
             borderColor: "rgba(28, 200, 138, 1)",
             pointRadius: 3,
             pointBackgroundColor: "rgba(78, 115, 223, 1)",
@@ -232,84 +155,10 @@ $(function () {
             pointHitRadius: 10,
             pointBorderWidth: 2,
             data: [totaldeaths6, totaldeaths5, totaldeaths4, totaldeaths3, totaldeaths2, totaldeaths1],
-          }],
-        },
-        options: {
-          maintainAspectRatio: false,
-          layout: {
-            padding: {
-              left: 10,
-              right: 25,
-              top: 25,
-              bottom: 0
-            }
-          },
-          scales: {
-            xAxes: [{
-              time: {
-                unit: 'date'
-              },
-              gridLines: {
-                display: false,
-                drawBorder: false
-              },
-              ticks: {
-                maxTicksLimit: 7
-              }
-            }],
-            yAxes: [{
-              ticks: {
-                maxTicksLimit: 5,
-                padding: 10,
-                // Include a dollar sign in the ticks
-                callback: function (value, index, values) {
-                  return number_format(value);
-                }
-              },
-              gridLines: {
-                color: "rgb(234, 236, 244)",
-                zeroLineColor: "rgb(234, 236, 244)",
-                drawBorder: false,
-                borderDash: [2],
-                zeroLineBorderDash: [2]
-              }
-            }],
-          },
-          legend: {
-            display: false
-          },
-          tooltips: {
-            backgroundColor: "rgb(255,255,255)",
-            bodyFontColor: "#858796",
-            titleMarginBottom: 10,
-            titleFontColor: '#6e707e',
-            titleFontSize: 14,
-            borderColor: '#dddfeb',
-            borderWidth: 1,
-            xPadding: 15,
-            yPadding: 15,
-            displayColors: false,
-            intersect: false,
-            mode: 'index',
-            caretPadding: 10,
-            callbacks: {
-              label: function (tooltipItem, chart) {
-                var datasetLabel = chart.datasets[tooltipItem.datasetIndex].label || '';
-                return datasetLabel + number_format(tooltipItem.yLabel);
-              }
-            }
-          }
-        }
-      });
-      var ctx = document.getElementById("myAreaChart3");
-      var myLineChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-          labels: [totalweek6, totalweek5, totalweek4, totalweek3, totalweek2, totalweek1],
-          datasets: [{
-            label: "Confirmed : ",
+          },{
+            label: "Deaths : ",
             lineTension: 0.3,
-            backgroundColor: "rgba(231, 74, 59, 0.5)",
+            backgroundColor: "rgba(255, 255, 255, 0)",
             borderColor: "rgba(231, 74, 59, 1)",
             pointRadius: 3,
             pointBackgroundColor: "rgba(78, 115, 223, 1)",
@@ -388,9 +237,7 @@ $(function () {
             }
           }
         }
-      });recovered
-
-
+      });
     });
 
 
